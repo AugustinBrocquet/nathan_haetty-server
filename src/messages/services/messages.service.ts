cae7326bec8a33a74fe0bfcdf65a0b05b755b2b9
Augustin Brocquet <augustinbrocquet@enaco.fr>
2019-10-07 16:49:31 +0200

    change message

--- src/messages/services/messages.service.ts
+++ src/messages/services/messages.service.ts
@@ -3,16 +3,28 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Message } from '../interfaces/message.interface';
 import { CreateMessageDto } from '../DTOs/create-message.dto';
+import { MailerService } from '@nest-modules/mailer';
 
 @Injectable()
 export class MessagesService {
 
-    constructor(@InjectModel('Message') private messageModel: Model<Message>) { }
+    constructor(@InjectModel('Message') private messageModel: Model<Message>, private readonly mailerService: MailerService) { }
 
     async create(createMessageDto: CreateMessageDto) {
 
         const createdMessage = new this.messageModel(createMessageDto);
-        return await createdMessage.save();
+        await createdMessage.save();
+        const emailResponse = await this
+            .mailerService
+            .sendMail({
+                to: 'augustin.brocquet@gmail.com',
+                from: 'noreply@nestjs.com',
+                subject: 'Nouveau Message NathanHaetty.com✔',
+                text: `Bonjour, vous avez reçu un nouveau message : ${createdMessage.content}`,
+                html: `Bonjour, vous avez reçu un nouveau message :<br /> ${createdMessage.content}`,
+            });
+
+        return [createdMessage, emailResponse];
 
     }
 
