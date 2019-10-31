549af8a5923be8be166243b4e9dd319ed7432bc8
Augustin Brocquet <augustin@MacBook-Pro-de-Augustin.local>
2019-10-31 09:43:30 +0100

    change intagram

--- src/instagram/instagram.controller.ts
+++ src/instagram/instagram.controller.ts
@@ -9,11 +9,20 @@ export class InstagramController {
 
     constructor() {
         // Create a new instance.
+        // this.instagram = new Instagram({
+        //     clientId: '5ebf70a29ef74bf6a87882cd8f5e68a1',
+        //     clientSecret: '703aae9e3fe04a2eaf078280d303d236',
+        //     accessToken: '1176616020.5ebf70a.7c1e2e024e714a48a9a13a111e61eaa3',
+        // });
+
         this.instagram = new Instagram({
-            clientId: '5ebf70a29ef74bf6a87882cd8f5e68a1',
-            clientSecret: '703aae9e3fe04a2eaf078280d303d236',
-            accessToken: '1176616020.5ebf70a.7c1e2e024e714a48a9a13a111e61eaa3',
+            clientId: 'de9ed4b804234412948705392994cb13',
+            clientSecret: '47b5efd431bc46ab84c260abf48f1900',
+            accessToken: '1974355208.de9ed4b.8ec285ea76b6464488958d1c8918017e',
         });
+        
+        
+        
 
     }
 
