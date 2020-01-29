7211053e4badae9a618c3e48b9caefa73742a8f6
proga404dev <proga.404.dev@gmail.com>
2020-01-29 18:47:43 +0100

    finish upload

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -46,26 +46,35 @@ export class PostsController {
     // return [files.picture, files.sub_pictures, createPostDto];
 
     createPostDto.picture = files.picture[0].originalname;
-
+    // one file
     const ftp = new PromiseFtp();
-    ftp.connect({ host: 'ftp.cluster020.hosting.ovh.net', user: 'nathanhajh', password: 'Bhu8Nji9456' })
-      .then((serverMessage) => {
-        return ftp.put(`${__dirname}/../../resources/img/${files.picture[0].originalname}`, `/www/uploads/img/${files.picture[0].originalname}`);
-      }).then(() => {
-        return ftp.end();
-      });
     const pathsSubPictures = [];
 
+    const outputFiles = [];
+
+    if (files.picture) {
+      outputFiles.push(files.picture[0]);
+    }
+
     if (files.sub_pictures) {
-      files.sub_pictures.forEach(file => {
-        pathsSubPictures.push(file.originalname);
-        ftp.connect({ host: 'ftp.cluster020.hosting.ovh.net', user: 'nathanhajh', password: 'Bhu8Nji9456' })
-          .then((serverMessage) => {
-            return ftp.put(`${__dirname}/../../resources/img/${file.originalname}`, `/www/uploads/img/${file.originalname}`);
-          }).then(() => {
-            return ftp.end();
-          });
+      files.sub_pictures.forEach(element => {
+        pathsSubPictures.push(element.originalname);
+        outputFiles.push(element);
       });
+    }
+
+
+    // multi files
+    if (files.sub_pictures) {
+      ftp.connect({ host: 'ftp.cluster020.hosting.ovh.net', user: 'nathanhajh', password: 'Bhu8Nji9456' })
+        .then(() => this.multiPutFiles(outputFiles, ftp))
+        .then(() => {
+          // clear cache here
+          console.log('disconnecting...')
+          return ftp.end();
+        })
+        // connection errors
+        .catch((err) => { console.log(err.toString()) })
     }
 
     createPostDto.sub_pictures = pathsSubPictures;
@@ -98,35 +107,39 @@ export class PostsController {
     ),
   )
   @Put('/update')
-  @UseGuards(AuthGuard())
+  // @UseGuards(AuthGuard())
   async updatePost(@UploadedFiles() files, @Body() updatePostDto: any) {
     const ftp = new PromiseFtp();
-    if (files.picture) {
-      updatePostDto.picture = files.picture[0].originalname;
-
-      ftp.connect({ host: 'ftp.cluster020.hosting.ovh.net', user: 'nathanhajh', password: 'Bhu8Nji9456' })
-        .then((serverMessage) => {
-          return ftp.put(`${__dirname}/../../resources/img/${files.picture[0].originalname}`, `/www/uploads/img/${files.picture[0].originalname}`);
-        }).then(() => {
-          return ftp.end();
-        });
-    }
-
+    const outputFiles = [];
     const pathsSubPictures = [];
 
-    if (files.sub_pictures) {
-      files.sub_pictures.forEach(file => {
-        pathsSubPictures.push(file.originalname);
-        ftp.connect({ host: 'ftp.cluster020.hosting.ovh.net', user: 'nathanhajh', password: 'Bhu8Nji9456' })
-          .then((serverMessage) => {
-            return ftp.put(`${__dirname}/../../resources/img/${file.originalname}`, `/www/uploads/img/${file.originalname}`);
-          }).then(() => {
-            return ftp.end();
-          });
-      });
-      updatePostDto.sub_pictures = pathsSubPictures;
+    if (files.picture) {
+      outputFiles.push(files.picture[0]);
     }
 
+    if (files.sub_pictures) {
+      files.sub_pictures.forEach(element => {
+        pathsSubPictures.push(element.originalname);
+        outputFiles.push(element);
+      });
+    }
+
+
+
+    // multi files
+    if (files.sub_pictures) {
+      ftp.connect({ host: 'ftp.cluster020.hosting.ovh.net', user: 'nathanhajh', password: 'Bhu8Nji9456' })
+        .then(() => this.multiPutFiles(outputFiles, ftp))
+        .then(() => {
+          // clear cache here
+          console.log('disconnecting...')
+          return ftp.end();
+        })
+        // connection errors
+        .catch((err) => { console.log(err.toString()) })
+    }
+
+    updatePostDto.sub_pictures = pathsSubPictures;    // return [createPostDto];
     return await this.postsService.updatePost(updatePostDto);
   }
 
@@ -139,5 +152,23 @@ export class PostsController {
   // @UseGuards(AuthGuard())
   async deleteSubPicture(@Body() data: any) {
     return await this.postsService.deleteSubPicture(data);
+  }
+
+  multiPutFiles(fileList, ftp) {
+    return new Promise((resolve, reject) => {
+      var chain = Promise.resolve();
+
+      fileList.forEach(function (file, i, arr) {
+        chain = chain.then(() => {
+          console.log('uploading:' + file.originalname);
+          return ftp.put(__dirname + '/../../resources/img/' + file.originalname, '/www/uploads/img/' + file.originalname);
+        })
+          // file upload errors
+          .catch((err) => { console.log(err.toString()) })
+
+        if (i === arr.length - 1)
+          chain.then(() => resolve())
+      })
+    })
   }
 }
